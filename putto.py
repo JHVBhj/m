c8590688f66da5344bed9b9a5dbfb339879c9b3c
JHVBhj <91900128+JHVBhj@users.noreply.github.com>
2022-01-03 18:41:30 +0100

    Create putto.py

---
+++ putto.py
@@ -0,0 +1,17 @@
+import random
+listamegj=[];
+listahuzott=[];
+
+megjatszott4=input("Megjatszott szamok 4-bol: ")
+listamegj.append(megjatszott4)
+for i in range(8):
+    megjatszott8=input("Megjatszott szamok 20-bol: ")
+    listamegj.append(megjatszott8)
+
+
+
+huzott4=random.randint(1,4)
+listahuzott.append(huzott4)
+for i in range(8):
+    
+    
